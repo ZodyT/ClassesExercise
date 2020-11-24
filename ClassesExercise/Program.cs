bdcbd6ffd34bdbaf704b64c82763715bf93801af
ZodyT <Zoraida1988@yahoo.com>
2020-11-24 15:21:23 -0600

    Finished

---
+++ ClassesExercise/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ClassesExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myCar = new Car();
+
+            myCar.Make = "Chevrolet";
+            myCar.Model = "Classic";
+            myCar.Year = 2004;
+
+            Console.WriteLine($"{myCar.Year} {myCar.Make} {myCar.Model}");
+        }
+    }
+}
